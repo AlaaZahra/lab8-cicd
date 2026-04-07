7d5a9569efec4582fe444239fc864dfd73553b2a
Alaa Zahra <alaaabdelmaksod2002@gmail.com>
2026-04-07 15:51:37 +0200

    Task 3: Testing Automated Update (Milk to Coffee)

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 
 const tasks = [
-  { id: 1, name: 'Milk',          status: 'done'    },
+  { id: 1, name: 'CoffeeCoffee for Lab 8',          status: 'done'    },
   { id: 2, name: 'Eggs',          status: 'done'    },
   { id: 3, name: 'Bread',         status: 'pending' },
   { id: 4, name: 'Butter',        status: 'pending' },

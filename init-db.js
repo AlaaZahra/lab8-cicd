c0c59944dc4a69f3d33bc8a449e7181029fb679e
Alaa Zahra <alaaabdelmaksod2002@gmail.com>
2026-04-07 13:37:00 +0200

    Task 3: Changed Milk to Coffee

--- init-db.js
+++ init-db.js
@@ -1,7 +1,7 @@
 db = db.getSiblingDB('todolist');
 db.tasks.drop();
 db.tasks.insertMany([
-  { id: 1, name: 'Buy Milk', status: 'pending' },
+  { id: 1, name: 'Buy Coffee for Lab 8', status: 'pending' },
   { id: 2, name: 'Clean Room', status: 'completed' },
   { id: 3, name: 'Finish Lab 8', status: 'pending' }
 ]);
